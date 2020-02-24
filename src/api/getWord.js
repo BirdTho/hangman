46cd0e6141401523c35e2eaceb21767229c8a3a2
Christopher Thomas <cthomas@netsertive.com>
2020-02-23 22:12:39 -0800

    Replace unreliable word api with word list file, include credits for it.

---
+++ src/api/getWord.js
@@ -0,0 +1,19 @@
+import * as data from './words_dictionary.json';
+
+class WordApi {
+  /**
+   * @param {Object} words
+   */
+  constructor(words) {
+    this.words = Object.keys(words.default);
+    this.length = this.words.length;
+  }
+
+  getWord() {
+    return this.words[Math.floor(Math.random() * this.length)];
+  }
+}
+
+const api = new WordApi(data);
+
+export default api;
